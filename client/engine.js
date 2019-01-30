1b48b5e0910560081fff3c78361ebed9ac9570f4
ShadowCat7 <lukasattridge@gmail.com>
2019-01-30 12:09:42 -0700

    clean up engine.js a bit

--- client/engine.js
+++ client/engine.js
@@ -1,27 +1,26 @@
 const EPSILON = 0.00000001;
 const TARGET_FPS = 60;
-const TARGET_SPF = 1 / TARGET_FPS;
 const FPS_SMOOTHNESS = 0.9;
 const FPS_ONE_FRAME_WEIGHT = 1.0 - FPS_SMOOTHNESS;
 const MIN_FPS = 20;
 const MAX_SPF = 1 / MIN_FPS;
 
 function Engine(canvas, updateFunc, drawFunc) {
-    var self = this;
-    var animationFrameId = null;
-    var timeoutId = null;
+    let self = this;
+    let animationFrameId = null;
+    let timeoutId = null;
     const buttonsPressed = {};
-    var keysPressedLabel = document.getElementById('keypressed');
+    const keysPressedLabel = document.getElementById('keypressed');
 
     self.fps = TARGET_FPS;
 
     function mainLoop() {
-        var previousUpdate = new Date().getTime();
-        var previousDraw = new Date().getTime();
+        let previousUpdate = new Date().getTime();
+        let previousDraw = new Date().getTime();
 
         function update() {
-            var currentTime = new Date().getTime();
-            var timeSinceLastUpdate = (currentTime - previousUpdate) / 1000;
+            let currentTime = new Date().getTime();
+            let timeSinceLastUpdate = (currentTime - previousUpdate) / 1000;
             previousUpdate = currentTime;
 
             if (timeSinceLastUpdate < EPSILON)
@@ -37,10 +36,10 @@ function Engine(canvas, updateFunc, drawFunc) {
         }
 
         function draw() {
-            var currentTime = new Date().getTime();
-            var timeSinceLastUpdate = (currentTime - previousUpdate) / 1000;
-            var timeSinceLastDraw = (currentTime - previousDraw) / 1000;
-            var fps = 1 / timeSinceLastDraw;
+            let currentTime = new Date().getTime();
+            let timeSinceLastUpdate = (currentTime - previousUpdate) / 1000;
+            let timeSinceLastDraw = (currentTime - previousDraw) / 1000;
+            let fps = 1 / timeSinceLastDraw;
             previousDraw = currentTime;
 
             self.fps = self.fps * FPS_SMOOTHNESS + fps * FPS_ONE_FRAME_WEIGHT;
@@ -62,7 +61,7 @@ function Engine(canvas, updateFunc, drawFunc) {
     self.stop = function () {
         cancelAnimationFrame(animationFrameId);
         clearTimeout(timeoutId);
-    }
+    };
 
     document.addEventListener('keydown', function (e) {
         e.stopPropagation();
